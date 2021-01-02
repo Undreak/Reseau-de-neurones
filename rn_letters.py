c99968f9af5c06d01119e1bd36f6dbefec6c1e32
Undreak <alexandre.poke@live.fr>
2021-01-02 21:29:08 +0100

    corrections

--- rn_letters.py
+++ rn_letters.py
@@ -9,8 +9,8 @@ images_letters, labels_letters = extract_training_samples('letters')
 start = time.time()
 def learn(mat):
     # cette fonction permet d'apprendre une lettre
-    # plus un pixel sera present et plus sa valeur sera elevee
-    # a l'inverse moins un pixel sera present et plus sa valeur sera faibles voir meme negative
+    # plus un pixel sera present et plus sa valeur sera elevée
+    # à l'inverse moins un pixel sera present et plus sa valeur sera faible
     for i in range(im_size):
         for j in range(im_size):
             if mat[i,j] == 1:
@@ -22,13 +22,13 @@ def learn(mat):
     return mat
 
 def readim(im):
-    # cette fonction permet de lire une image de taille im_size x im_size et de la binarizer
+    # cette fonction permet de lire une image de taille im_size x im_size et de la transformer en image binaire
     th = 128    #valeur seuil
-    im_bool = (im > th)     # test boolean pour ne garder que les valeurs au dessus d'une valeur seuil 
+    im_bool = (im > th)     # test booléen pour ne garder que les valeurs au dessus d'une valeur seuil 
     mat = np.zeros((im_size, im_size))  # on definit la matrice binaire de sortie
     for i in range(im_size):
         for j in range(im_size):
-            # on scan toute l'image pour regarder si un element de matrice est True ou False 
+            # on scanne toute l'image pour regarder si un élément de matrice est True ou False 
             # puis on lui attribut une valeur: True => 1 et False => 0
             if im_bool[i,j]:
                 mat[i,j] = 1
@@ -37,39 +37,37 @@ def readim(im):
     return mat
 
 def Q(image):
-    # cette fonction permet de calculer le score d'une image et de determiner de quelle lettre il s'agit
+    # cette fonction permet de calculer le quotient de reconnaissance d'une image 
+    # et de determiner de quelle lettre il s'agit
     I = image
     phik = np.zeros(k)  # score candidat
     muk = np.zeros(k)   # score idéal du modèle de poids
     Qk = np.zeros(k)    # quotient de reconnaissance
 
     for i in range(k):
-        # on calcul ce quotient pour chacune des lettre a reconnaitre
+        # on calcule ce quotient pour chacune des lettres à reconnaître
         for j in range(im_size):
             for l in range(im_size):
-                # on scan l'image pour calculer le produit matricielle du poid Wk et de l'image I
+                # on calcule le produit matricielle du poid Wk et de l'image I
                 phik[i] += Wk[i][j][l]*I[j][l]
                 if Wk[i][j][l] > 0:
-                    # on calcule la somme de tous les elements positif de Wk
+                    # on calcule la somme de tous les êlêments positif de Wk
                     muk[i] += Wk[i][j][l]
-        # on calcul le quotient de reconnaire pour une lettre k
+        # on calcule le quotient de reconnaire pour une lettre k
         Qk[i] = phik[i]/muk[i]
     return np.argmax(Qk)    # renvoie la lettre avec le plus haut quotient de reconnaissance
 
 
-# alphabet utilise, les lettres trop similaire entre majuscule/minuscule sont considere comme etant une seule lettre
+# alphabet utilisé 
 alphabet = np.array(['A','B','C','D','E','F','G','H','I','J','K','L','M',
                     'N','O','P','Q','R','S','T','U','V','W','X','Y','Z'])
 
-num_images = 100000
+num_images = len(labels_letters)
 im_size = 28
 k = 26
-Qkreal = np.zeros((k,num_images))  # statistique memorisant les choix de Q(images) pour identifier les erreurs
-
 Wk = np.zeros((k,im_size,im_size))
 for i in range(num_images):
-    # ici on calcul la matrice de poid pour chacune des lettres en utilisant le dataset balanced 
-    # on ignore les valeurs < 9 car ceux sont des chiffres dans ce dataset
+    # ici on calcul la matrice de poid pour chacune des lettres en utilisant le dataset letters 
     Wk[int(labels_letters[i]) - 1] += learn(readim(images_letters[i]))
     
 end = time.time()
@@ -77,24 +75,26 @@ print(f"Temps d'execution du programme: {end - start} s\n")
 
 images_letters_test, labels_letters_test = extract_test_samples('letters')
 
-num_images_test = 20800  # Test verifiant si le reseau de neuronnes est performant
-Qstat = 0   # statistique nous donnant le pourcentage de reussite du modele
-Qkstat = np.zeros(k)    # la meme mais pour chacune des lettres individuelle
+# Test verifiant les performances du réseau de neurones
+num_images_test = len(labels_letters_test)
+Qstat = 0   # statistique nous donnant le pourcentage de reussite du modèle
+Qkstat = np.zeros(k)    # la même mais pour chacune des lettres individuelle
 kk = np.zeros(k)    # nombre permettant de normaliser Qkstat
-Qkreal = np.zeros((k,num_images_test))  # statistique memorisant les choix de Q(images) pour identifier les erreurs
+Qkreal = np.zeros((k,num_images_test)) - 1 # statistique mémorisant les choix de Q(images) pour identifier les érreurs
 
 for i in range(int(num_images_test - 1)):
     if Q(images_letters_test[i]) == int(labels_letters_test[i] - 1): 
         # on regarde si le resultat de Q(image) est correct et correspond au bon label
-        # on incremente Qstat/Qkstat a chaque fois que Q(images) est correct
+        # on incrémente Qstat et Qkstat a chaque fois que Q(images) est correct
         Qstat += 1
         Qkstat[int(labels_letters_test[i] - 1)] += 1
     kk[int(labels_letters_test[i] - 1)] += 1  # on calcule la norme de Qkstat
-    Qkreal[Q(images_letters_test[i])][i] = labels_letters_test[i] 
-        # on memorise le bon label de la lettre que Q(image) pense etre le bon resultat
+    Qkreal[int(labels_letters_test[i] - 1)][i] = Q(images_letters_test[i])
+    # on mémorise le label renvoyé par Q(image) dans le bon label de Qkreal
+    # distribution des lettres
 
 end = time.time()
-print(f"Temps d'execution du programme: {end - start} s")
+print(f"Temps d'execution du programme: {end - start} s\n")
 
 # AFFICHAGE DES RESULTATS
 print(Qstat/num_images_test)
@@ -107,18 +107,18 @@ plt.clf()
 xk = np.zeros(k)
 for i in range(len(Qkreal)):
     for j in range(len((Qkreal[i]))):      
-        if Qkreal[i][j] > 0:
+        if Qkreal[i][j] >= 0:
             xk[i] += 1
 
 for i in range(k):
     n = 0
     y = np.zeros(int(xk[i]))
     for j in range(len(Qkreal[i])):
-        if Qkreal[i][j] > 0:
+        if Qkreal[i][j] >= 0:
             y[n] = Qkreal[i][j]
             n += 1
     N, bins, pathces = plt.hist(y,bins=k)
-    plt.xticks(np.arange(1,1+k),alphabet)
+    plt.xticks(np.arange(0,k),alphabet)
     plt.title(alphabet[i])
     plt.savefig('images/' + alphabet[i] + '_letters.png')
     plt.close()
